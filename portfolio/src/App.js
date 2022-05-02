8db1656955d0172b29e826099d71a663a487c275
bryanhuffstutler <bhuff8404@gmail.com>
2022-05-01 20:57:47 -0500

    tools drop down

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react'
+import Tools from './components/Tools'
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+
+      
+      <Tools />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import {GiHamburgerMenu} from 'react-icons/gi'
+
+function Navbar() {
+    return (
+        <div id='navbar'>
+            <GiHamburgerMenu size='2em'/>
+        </div>
+    );
+}
+
+export default Navbar;
