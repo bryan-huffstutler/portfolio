acf38c4c84de6a562df86d7fd9721d88cc6d9e94
bryanhuffstutler <bhuff8404@gmail.com>
2022-05-24 11:12:08 -0500

    imagereel

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,14 +1,20 @@
 import React from 'react'
-
 import ImageReel from './ImageReel/ImageReel'
-import { bbs, rtv } from './ImageReel/image-data
+import { bbs, rtv } from './ImageReel/image-data'
 
 function Projects(props) {
 
 
     return (
         <div>
-            {bbs.map((x, index) => <img key={index} src={x.source}/>)}
+            <div>
+                <ImageReel imgReel={bbs} />
+            </div>
+
+            <div>
+                <ImageReel imgReel={rtv} />
+            </div>
+
 
         </div>
     );
