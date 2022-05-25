b38233a103683a8bff3fd562729e7f1bd0c0ab41
bryanhuffstutler <bhuff8404@gmail.com>
2022-05-24 22:26:38 -0500

    cv download

--- portfolio/src/components/Intro.js
+++ portfolio/src/components/Intro.js
@@ -1,6 +1,7 @@
 import pictureOfMe from '../pic.jpg'
 import {FaLinkedin,FaGithub} from 'react-icons/fa'
 import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineDownload} from 'react-icons/ai'
 
 function Intro() {
   return ( 
@@ -10,6 +11,7 @@ function Intro() {
       <a href="https://www.linkedin.com/in/bryan-huffstutler-637730197/" target='_blank'><FaLinkedin size='2em' color='#0A66C2'/></a>
       <a href="https://github.com/bryan-huffstutler" target='_blank'><FaGithub size='2em' color='#161B22'/></a>
       <a href="mailto:bhuff8404@gmail.com"><AiOutlineMail size='2em' color='green'/></a>
+      <a href='Bryan Huffstutler.pdf' download="BryanHuffstutler.pdf"><AiOutlineDownload size='2em'/>Resume</a>
     </div>
    );
 }
